a114dfb2b69e1b21b6e6a1b6f2a972ae12f5546b
GibranAja <mgibranarraffi@gmail.com>
2026-04-18 21:28:36 +0700

    feat(preorder): Add PreOrderHome constants
    - Define pre-order operation constants
    - Configure API endpoints

---
+++ resources/js/constants/Admin/preOrderHome/index.ts
@@ -0,0 +1,6 @@
+import type { PreOrderStatus } from '@/types/Admin/preOrderHome';
+
+export const PRE_ORDER_DEFAULT_STATUS: PreOrderStatus = 'buka';
+export const PRE_ORDER_DEFAULT_MAX_SLOTS = 24;
+export const PRE_ORDER_DEFAULT_FILLED_SLOTS = 0;
+export const PRE_ORDER_DEFAULT_COUNTDOWN_HOURS = 24;
