54e4d846fe3c2c1de07a4ff452592bcfe988c86b
rhelzz <muhammadrasyad1004@gmail.com>
2026-04-18 09:11:20 +0700

    Commit : add page head title for admin carousel home

--- resources/js/pages/Admin/CarouselHome.vue
+++ resources/js/pages/Admin/CarouselHome.vue
@@ -1,5 +1,6 @@
 <template>
     <AdminLayout>
+        <Head title="Admin Carousel Home" />
         <div class="admin-page">
             <AdminPageHeader
                 title="Kelola Carousel Home"
@@ -227,6 +228,7 @@
 </template>
 
 <script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
 import { computed, reactive, ref } from 'vue';
 
 import AdminAlert from '@/components/admin/AdminAlert.vue';
