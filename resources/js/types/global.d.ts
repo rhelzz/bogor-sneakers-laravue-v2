2046eb101f4d61c3ae9f8781372f2659309c5be9
rhelzz <muhammadrasyad1004@gmail.com>
2026-04-18 08:44:57 +0700

    Commit : Extend global shared props typing for floating contacts

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,4 +1,5 @@
 import type { Auth } from '@/types/auth';
+import type { FloatingContact } from '@/types/floating-ui';
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
@@ -19,6 +20,7 @@ declare module '@inertiajs/core' {
             name: string;
             auth: Auth;
             sidebarOpen: boolean;
+            floatingContacts: FloatingContact[];
             [key: string]: unknown;
         };
     }
